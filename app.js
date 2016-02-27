732d48cd4880a09c0c2f8466ed1ecff6c0dda010
Hector Leon <hleon57@gmail.com>
2016-02-26 16:23:17 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,66 @@
+var request = require('request');
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var app = express();
+var server = require('http').Server(app);
+
+var urlBrewery = 'http://beermapapi.azurewebsites.net/api/brewery'; //
+var urlRendezvous = 'http://beermapapi.azurewebsites.net/api/rendezvous';
+
+var bIds =[];
+var i=0;
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req,res){
+
+	request(urlBrewery, function (error, response, body){
+		if (!error && response.statusCode == 200){
+			var brewerys = [];
+			data = JSON.parse(body);
+			data.Data.forEach(function (brew){
+				brewerys[i] = {
+					name: brew.Name,
+					geo: brew.Geolocation,
+					dir: brew.Direction,
+					id: brew.BreweryId
+				}
+				i++;
+			});
+		i=0;
+
+		request(urlRendezvous, function (error, response, body){
+			if (!error && response.statusCode == 200){
+				var rendezvous = [];
+				data = JSON.parse(body);
+				data.Data.forEach(function (rend){
+					rendezvous[i] = {
+						name: rend.Name,
+						geo: rend.Geolocation,
+						id: rend.RendezvousId
+					}
+				i++;
+				});
+		i=0;	
+
+				rendezvous = JSON.stringify(rendezvous);
+				brewerys = JSON.stringify(brewerys);
+				res.render('index', {brewerys: brewerys, rendezvous: rendezvous});
+			}
+
+			
+		});
+
+		}
+		else
+			res.send(error);
+	});
+
+	    	
+});
+
+server.listen(3000, function(){
+	console.log('Server running');
+});
